d716b9c130fd55e22a52407f5312491c9e21c7c6
DataTinkerer / DataNikkari <jarmo.tahvanainen@gmail.com>
2024-12-18 13:52:14 +0200

    add additional items to the main list

--- lists/lists.py
+++ lists/lists.py
@@ -2,6 +2,9 @@ def main():
     my_list = []
 
     my_list.append("read the python for data science book")
+    my_list.append("clean house")
+    my_list.append("learn python")
+    my_list.append("buy groceries")
 
     print(my_list)
 
