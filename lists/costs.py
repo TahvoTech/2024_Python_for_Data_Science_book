f68029606428bad279363c8efeb5dca1184b14ba
DataTinkerer / DataNikkari <jarmo.tahvanainen@gmail.com>
2025-01-02 14:42:03 +0200

    add costs.py to track and display monthly expenses

---
+++ lists/costs.py
@@ -0,0 +1,11 @@
+costs = []
+
+costs.append("House loan monthly payment")
+costs.append("Electricity bill")
+costs.append("Water bill")
+costs.append("Internet bill")
+costs.append("Groceries")
+costs.append("Trash bags")
+
+for index, item in enumerate(costs):
+    print(f"Index {index}: {item}")
